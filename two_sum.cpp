1e770c37aaefc8999b2106f2deac708fe0f24b1f
puhao <yuanpuhao@gmail.com>
2013-08-28 20:44:16 +0800

    取巧直接用hash

--- two_sum.cpp
+++ two_sum.cpp
@@ -25,7 +25,66 @@ struct Elem
 	Elem(int val,int pos):val(val),pos(pos)	{}
 };
 
+vector<int> twoSum(vector<int> &numbers, int target) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        map<int,int> HashPos;
+        vector<int>::const_iterator it;
+        vector<int> result;
+        int one = 0;
+        int second = 0;
+        int half = target/2;
+        int index = 1;
+        for(it=numbers.begin();it<numbers.end();it++)
+        {
+        	if (*it == half)
+        	{
+        		if (one == 0)
+        			one = index;
+        		else
+        			second = index;
+        	}
+        	HashPos[*it] = index++;
+        }
+        if (one>0 && second>0)
+        {
+        	result.push_back(one);
+        	result.push_back(second);
+        	return result;
+        }
+        map<int,int>::const_iterator MapIt;
+        for(MapIt=HashPos.begin();MapIt!=HashPos.end();MapIt++)
+        {
+        	if (HashPos.count(target - MapIt->first))
+        	{
+        		int one = MapIt->second;
+        		int second = HashPos[target - MapIt->first];
+        		if (one < second)
+        		{
+        			result.push_back(one);
+        			result.push_back(second);
+        		}
+        		else
+        		{
+        			result.push_back(second);
+        			result.push_back(one);
+        		}
+        		break;
+        	}
+        }
+        return result;
+
+    }
+
 int main()
 {
+	int nums[] = {2,7,11,15};
+	int target = 9;
+	vector<int> v;
+	for (int i=0;i<4;i++)
+		v.push_back(nums[i]);
+	vector<int> r;
+	r = twoSum(v,target);
+	cout << r[0] << " " << r[1] << endl;
 	return 0;
 }
