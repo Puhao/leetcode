f49544fce4a1777a6f92d7adca3866f3b57a7735
puhao <yuanpuhao@gmail.com>
2013-09-21 19:36:29 +0800

    DP

--- Wildcard_Matching.cpp
+++ Wildcard_Matching.cpp
@@ -6,35 +6,47 @@ using namespace std;
 bool isMatch(const char *s, const char *p) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-	if (*p == 0)
-		return *s == 0;
-	if (*p == '?')
+	int l1 = strlen(s);
+	int l2 = strlen(p);
+	bool Ret[l1+1][l2+1];
+	memset(Ret,false,sizeof(Ret)/sizeof(Ret[0][0]));
+	Ret[0][0] = true;
+	for(int j=1;j<l2+1;j++)
 	{
-		if (strlen(s) < 1)
-			return false;
-		else
-			return isMatch(s+1,p+1);
+		if(*(p+l2-j) == '*')
+			Ret[0][j] = Ret[0][j-1];
 	}
-	if (*p == '*')
+	for(int i=1;i<l1+1;i++)
 	{
-		for (int i=0;i<strlen(s)+1;i++)
+		for(int j=1;j<l2+1;j++)
 		{
-			if (isMatch(s+i,p+1))
-				return true;
+			if (*(p+l2-j) == '?')
+			{
+				Ret[i][j] = Ret[i-1][j-1];
+			}
+			else if (*(p+l2-j) == '*')
+			{
+				for(int k=0;k<=i;k++)
+				{
+					if(Ret[k][j-1] == true)
+					{
+						Ret[i][j] = true;
+						break;
+					}
+				}
+			}
+			else if (*(p+l2-j) == *(s+l1-i))
+				Ret[i][j] = Ret[i-1][j-1];
 		}
-		return false;
 	}
-	if (*p == *s)
-		return isMatch(s+1,p+1);
-	return false;
-
+	return Ret[l1][l2];
 }
 
 
 int main()
 {
-	char s1[] = "aaabbbaabaaaaababaabaaabbabbbbbbbbaabababbabbbaaaaba";
-	char s2[] = "a*******b";
+	char s1[] = "fafjlajfljfoaaafldef";
+	char s2[] = "f*aa*def";
 	cout << isMatch(s1,s2) << endl;
 	return 0;
 }
